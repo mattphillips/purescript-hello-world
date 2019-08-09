177bdb5b656b1ae2b63117d3495c1452bceb1d5c
Matt Phillips <matt@mattphillips.io>
2019-08-09 21:21:52 +0100

    Add update function to person repo algebra

--- src/Main.purs
+++ src/Main.purs
@@ -1,6 +1,6 @@
 module Main (main) where
 
-import Data.HashMap (HashMap, empty, insert, lookup)
+import Data.HashMap as HM
 import Data.Hashable (class Hashable)
 import Data.Maybe (Maybe)
 import Data.Number.Format (toString)
@@ -22,30 +22,39 @@ type Person a = { name :: Name, id :: a }
 
 type PersonRepo m =
   { create :: forall a. Person a -> m (Person PersonId)
-  , get    :: PersonId -> m (Maybe(Person PersonId))
+  , get    :: PersonId -> m (Maybe (Person PersonId))
+  , update :: PersonId -> (forall a. Person a -> Person a) -> m (Maybe (Person PersonId))
   }
 
-type Store = Ref (HashMap PersonId (Person PersonId))
+type Store = Ref (HM.HashMap PersonId (Person PersonId))
 
 createInMemoryPersonRepo :: Effect (PersonRepo Effect)
-createInMemoryPersonRepo = inMemoryPersonRepo <$> new empty
+createInMemoryPersonRepo = inMemoryPersonRepo <$> new HM.empty
   where
   inMemoryPersonRepo :: Store -> PersonRepo Effect
-  inMemoryPersonRepo store = { create, get }
+  inMemoryPersonRepo store = { create, get, update }
     where
     create :: forall a. Person a -> Effect (Person PersonId)
     create {name} = do
       id <- random
       let p = { name, id: PersonId(toString id) }
-      modify_ (\db -> insert p.id p db) store
+      modify_ (\db -> HM.insert p.id p db) store
       pure p
 
-    get :: PersonId -> Effect (Maybe(Person PersonId))
-    get id = (\db -> lookup id db) <$> read store
+    get :: PersonId -> Effect (Maybe (Person PersonId))
+    get id = (\db -> HM.lookup id db) <$> read store
+
+    update :: PersonId -> (forall a. Person a -> Person a) -> Effect (Maybe (Person PersonId))
+    update id f = do
+      person <- get id
+      modify_ (\db -> HM.update (\_ -> f <$> person) id db) store
+      pure person
 
 main :: Effect Unit
 main = do
   repo <- createInMemoryPersonRepo
   p <- repo.create { id: unit, name: Name("Matt") }
+  logShow p
+  _ <- repo.update p.id \pp -> { id: pp.id, name: Name("Matthew") }
   mp <- repo.get p.id
   logShow mp
