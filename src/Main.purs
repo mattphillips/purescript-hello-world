fce442959ccc409333b1b2dde78d19752c78616f
Matt Phillips <matt@mattphillips.io>
2019-08-09 18:56:11 +0100

    Add create/get API for person repo

---
+++ src/Main.purs
@@ -0,0 +1,49 @@
+module Main (main) where
+
+import Data.HashMap (HashMap, empty, insert, lookup)
+import Data.Hashable (class Hashable)
+import Data.Maybe (Maybe)
+import Data.Number.Format (toString)
+import Effect (Effect)
+import Effect.Console (logShow)
+import Effect.Random (random)
+import Effect.Ref (Ref, modify_, new, read)
+import Prelude (class Eq, class Show, Unit, bind, discard, pure, unit, (<$>))
+
+newtype Name = Name String 
+derive newtype instance showName :: Show Name
+
+newtype PersonId = PersonId String
+derive newtype instance showPersonId :: Show PersonId
+derive newtype instance eqPersonId :: Eq PersonId
+derive newtype instance hashablePersonId :: Hashable PersonId
+
+type Person a = { name :: Name, id :: a }
+
+type PersonRepo m =
+  { create :: forall a. Person a -> m (Person PersonId)
+  , get    :: PersonId -> m (Maybe(Person PersonId))
+  }
+
+type Store = Ref (HashMap PersonId (Person PersonId))
+
+inMemoryPersonRepo :: Store -> PersonRepo Effect
+inMemoryPersonRepo store = { create, get }
+  where
+  create :: forall a. Person a -> Effect (Person PersonId)
+  create {name} = do
+    id <- random
+    let p = { name, id: PersonId(toString id) }
+    modify_ (\db -> insert p.id p db) store
+    pure p
+
+  get :: PersonId -> Effect (Maybe(Person PersonId))
+  get id = (\db -> lookup id db) <$> read store
+
+main :: Effect Unit
+main = do
+  store <- new empty
+  let repo = inMemoryPersonRepo store
+  p <- repo.create { id: unit, name: Name("Matt") }
+  mp <- repo.get p.id
+  logShow mp
