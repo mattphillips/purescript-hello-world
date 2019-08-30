233c450431fb63d6df0fdc58c05eee4de9a3061c
Matt Phillips <matt@mattphillips.io>
2019-08-30 14:14:27 +0100

    Abstract in memory repository effect type

--- src/Todo/InMemoryInterpreters.purs
+++ src/Todo/InMemoryInterpreters.purs
@@ -1,9 +1,9 @@
 module Todo.InMemoryInterpreters (createInMemoryTodoRepo) where
 
-import Data.Function (const, (<<<), (>>>))
+import Data.Function (const, ($), (<<<), (>>>))
 import Data.HashMap as HM
 import Data.Maybe (Maybe)
-import Effect (Effect)
+import Effect.Class (class MonadEffect, liftEffect)
 import Effect.Ref as Ref
 import Id (genId)
 import Prelude (Unit, bind, discard, pure, (<$>), (==))
@@ -12,34 +12,34 @@ import Todo.Repository (Filter, TodoRepo)
 
 type Store = Ref.Ref (HM.HashMap TodoId (Todo TodoId))
 
-createInMemoryTodoRepo :: Effect (TodoRepo Effect)
-createInMemoryTodoRepo = inMemoryTodoRepo <$> Ref.new HM.empty
+createInMemoryTodoRepo :: forall m. MonadEffect m => m (TodoRepo m)
+createInMemoryTodoRepo = inMemoryTodoRepo <$> (liftEffect $ Ref.new HM.empty)
   where
-  inMemoryTodoRepo :: Store -> TodoRepo Effect
+  inMemoryTodoRepo :: Store -> TodoRepo m
   inMemoryTodoRepo store = { create, delete, get, getAll, filter, update }
     where
-    create :: Description -> Effect (Todo TodoId)
+    create :: Description -> m (Todo TodoId)
     create description = do
-      id <- genId
+      id <- liftEffect $ genId
       let t = Todo { description, id, isComplete: (IsComplete false) }
-      Ref.modify_ (HM.insert id t) store
+      liftEffect $ Ref.modify_ (HM.insert id t) store
       pure t
 
-    get :: TodoId -> Effect (Maybe (Todo TodoId))
-    get id = (HM.lookup id) <$> Ref.read store
+    get :: TodoId -> m (Maybe (Todo TodoId))
+    get id = (HM.lookup id) <$> (liftEffect $ Ref.read store)
 
-    getAll :: Effect (Array (Todo TodoId))
-    getAll = HM.values <$> Ref.read store
+    getAll :: m (Array (Todo TodoId))
+    getAll = liftEffect $ HM.values <$> Ref.read store
 
-    filter :: Filter -> Effect (Array (Todo TodoId))
-    filter f = (HM.values <<< (HM.filter (getIsComplete >>> (_ == f.isComplete))) <$> Ref.read store)
+    filter :: Filter -> m (Array (Todo TodoId))
+    filter f = (HM.values <<< (HM.filter (getIsComplete >>> (_ == f.isComplete))) <$> (liftEffect $ Ref.read store))
 
-    update :: TodoId -> (∀ a. Todo a -> Todo a) -> Effect (Maybe (Todo TodoId))
+    update :: TodoId -> (∀ a. Todo a -> Todo a) -> m (Maybe (Todo TodoId))
     update id f = do
       todo <- get id
       let updatedTodo = f <$> todo
-      Ref.modify_ (HM.update (const updatedTodo) id) store
+      liftEffect $ Ref.modify_ (HM.update (const updatedTodo) id) store
       pure updatedTodo
 
-    delete :: TodoId -> Effect Unit
-    delete id = Ref.modify_ (HM.delete id) store
+    delete :: TodoId -> m Unit
+    delete id = liftEffect $ Ref.modify_ (HM.delete id) store
